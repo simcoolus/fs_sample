f66253cb575e418563ddae3dbdddb4354fa22daa
Brett Bailey <brettb@webninja.com.au>
2020-01-03 08:09:31 +1000

    Commit of Requirements

---
+++ app/Http/Controllers/DummyController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DummyController extends Controller
+{
+    // TODO: Make an area where someone can add books they've bought
+    /*
+     * Books need a title, author, blurb, status (not started, started, finished, retired). Need a list of current books paginated, and a way to add/edit/remove books
+     */
+
+    // TODO: Make an api route where someone can get their books (needs to be filtered by current status only)
+
+    // TODO; Make an api route where someone can get the details of 1 book by id
+    /*
+     *
+     * https://laravel.com/docs/6.x/api-authentication - use the token guard here
+     */
+}
