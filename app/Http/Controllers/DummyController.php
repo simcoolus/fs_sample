19780b7ab433fcf10d0763a0722b8dc28cdfbddb
Simpson Moyo <simpsonmoyo@gmail.com>
2020-08-03 07:56:53 +1000

    Books test (APIs and web)

--- app/Http/Controllers/DummyController.php
+++ app/Http/Controllers/DummyController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class DummyController extends Controller
 {
+    //Simpson: refer to BooksFormContoller and BooksController
     // TODO: Make an area where someone can add books they've bought
     /*
      * Books need a title, author, blurb, status (not started, started, finished, retired). Need a list of current books paginated, and a way to add/edit/remove books
      */
 
+    // --------------------
+
+    //Simpson: Refer to BooksApiController
     // TODO: Make an api route where someone can get their books (needs to be filtered by current status only)
 
     // TODO; Make an api route where someone can get the details of 1 book by id
